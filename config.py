181fc6ed3a4c54fc59a22a6739c5f128f85289c9
Ryan Mohrman <ryan.mohrman@gmail.com>
2019-11-06 22:05:28 -0700

    Completely destroyed application... Ryan will fix after sleep

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-DEBUG = True
+config = {
+    "development": "soundstyles.config.DevelopmentConfig",
+    "testing": "soundstyles.config.TestingConfig",
+    "default": "soundstyles.config.DevelopmentConfig"
+}
+
+
+def configure_app(app):
+    config_name = os.getenv('FLASK_CONFIGURATION', 'default')
+    app.config.from_object(config[config_name]) # object-based default configuration
+    app.config.from_pyfile('config.cfg', silent=True) # instance-folders configuration
