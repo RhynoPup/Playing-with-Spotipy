0416ff3801cbc21316d6e4ffb5809ccd22cebe56
Ryan Mohrman <ryan.mohrman@gmail.com>
2019-11-06 20:41:41 -0700

    added all data files from initial playlist pull

--- static/dashboard/js/load_data.js
+++ static/dashboard/js/load_data.js
@@ -1,13 +1,19 @@
 $(function(){
 
-    var files = "../datafiles.json"
+    var datafile = "static/data/datafiles.json" //Direct Call
+    console.log(datafile)
     var promises = [];
-    d3.json(files).then(function(file){
-        promises.push(file)
+    d3.json(datafile).then(function(contents){
+        console.log(contents)
+        contents.forEach(function(item){
+            promises.push(d3.json(item))
+        })
+        
         return Promise.all(promises).then(function(data){
-            list_data=[]
+            
+            list_data=[];
             data.forEach(function(d){
-                list_dat.push(d)
+                list_data.push(d)
             })
             app_data = [].concat.apply([],list_data)
             console.log('App Data', app_data)
